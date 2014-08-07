291954d15b8dfe7f478c044fce3ea51a398691c9
Eric Huppert <eric@erichuppert.com>
2014-08-07 11:14:15 -0700

    brought in skin test stuff

---
+++ skin-test/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.circle').click(function(){
+    var target = $(this);
+    var parentContainer = target.closest(".question-wrap");
+    parentContainer.find(".circle").removeClass("selected");
+    var value = Number.parseInt(target.text());
+    parentContainer.find("input").val(value);
+    parentContainer.find(".check").removeClass("hide");
+    target.addClass("selected");
+    if (allComplete()) {
+      showScore();
+    }
+  })
+
+  function allComplete(){
+    var countSelected = $('.circle.selected').length;
+    var totalCount = $('.question-wrap').length;
+    return totalCount == countSelected;
+  }
+
+  function showScore() {
+    var score = 0;
+    $("input").each(function(){
+      score += Number.parseInt(this.value);
+    })
+    $("#number").text(score);
+    $('.result').removeClass('hide');
+  }
+})
